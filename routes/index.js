a718363a232b3277e063eb77163e3cb15a1dc29e
Perrym14 <malikap14@gmail.com>
2019-09-24 17:40:41 -0400

    Init

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+var messages = [{
+  text: 'Hey Ron',
+  user: 'Willy',
+  added: new Date()},
+  {
+  text: 'Hey Willberg',
+  user: 'Ron',
+  added: new Date()
+  }
+]
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express', messages: messages });
+});
+
+module.exports = router;
